5ecb191e65dc073a26b6d94db5011d697d7a637e
Graham <graham.moss@hotmail.com>
2020-03-02 16:55:28 -0500

    Styling mostly completed

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import Vote from './Vote';
 
 
 function Comment() {
   return (
     <div className="comment-box">
-      <img src ="https://www.placecage.com/c/100/100" class="avatar"></img>
-      <header>
+      <img src="https://www.placecage.com/c/50/50" alt ="avatar" className="avatar"></img>
+      <div className="header">
         <p className="header__name">Brad</p>
         <p className="header__info">AUTHOR</p>
-        <p className="header__time">2 minutes ago</p>
-      </header>
-        <div class="comment-box__content">
-          So what the German automaker is likely to focus on today is the bigger picture. This will be the first time we see the Taycan free from any prototype bodywork.
+        <p className="header__time">· 2 minutes ago</p>
+      </div>
+      <div className="comment-box__content">
+        So what the German automaker is likely to focus on today is the bigger picture. This will be the first time we see the Taycan free from any prototype bodywork.
         </div>
+      <div className="footer">
+        <p className="footer__name">Reply</p>
+        <p className="footer__info">21 REPLIES</p>
+        <Vote />
+      </div>
     </div>
   )
 }
